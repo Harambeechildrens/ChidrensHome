2ec3f790ccb7499aaf2a6fba684a389c7dbd5c36
Ali Wangara <Aliwangara63@gmail.com>
2025-03-16 18:30:15 +0300

    changed settings

--- harambe_home/harambe_home/settings.py
+++ harambe_home/harambe_home/settings.py
@@ -13,11 +13,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from django.core.wsgi import get_wsgi_application
 
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+application = get_wsgi_application()
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harambe_home.settings')
 
 
 # Quick-start development settings - unsuitable for production
