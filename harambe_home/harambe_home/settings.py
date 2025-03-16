d38b55d09301fc563ece957abe53070404ca04a9
Ali Wangara <Aliwangara63@gmail.com>
2025-03-16 17:57:16 +0300

    Did more changes

--- harambe_home/harambe_home/settings.py
+++ harambe_home/harambe_home/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chidrenshome.onrender.com/','127.0.0.1:8000','127.0.0.1']
 
 
 
