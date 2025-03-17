db767ef859a9b1788d37bdabd274ac8de5009017
Ali Wangara <Aliwangara63@gmail.com>
2025-03-17 12:42:27 +0300

    Added some changes to wsgi

--- harambe_home/harambe_home/wsgi.py
+++ harambe_home/harambe_home/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harambe_home.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harambe_home.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harambe_home/harambe_home.settings')
+
 
 application = get_wsgi_application()
 
